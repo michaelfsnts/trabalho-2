bfcbde52cf3bdf335d0baf9242759909c49ee4e3
Michael Ferreira dos Santos <64515823+michaelfsnts@users.noreply.github.com>
2022-12-07 13:58:48 -0300

    Add files via upload

---
+++ protejoSpotify/source/data.js
@@ -0,0 +1,88 @@
+export default[
+    //Primeira
+    {
+      title: "Go Down Swinging (Instrumental)",
+      artist: "NEFFEX",
+      cover: "imagemPadrao.png",
+      file: "Go Down Swinging (Instrumental) - NEFFEX.mp3"
+    },
+    //Segunda
+    {
+      title: "Hustlin' (Instrumental)",
+      artist: "NEFFEX",
+      cover: "imagemPadrao.png",
+      file: "Hustlin' (Instrumental) - NEFFEX.mp3"
+    },
+    //Terceira
+    {
+      title: "Just Breathing (Instrumental)",
+      artist: "NEFFEX",
+      cover: "imagemPadrao.png",
+      file: "Just Breathing (Instrumental) - NEFFEX.mp3"
+    },
+    //Quarta
+    {
+      title: "That's What It Takes (Instrumental)",
+      artist: "NEFFEX",
+      cover: "imagemPadrao.png",
+      file: "That's What It Takes (Instrumental) - NEFFEX.mp3"
+      },
+    //Quinta
+      {
+        title: "Dançarina",
+        artist: "Pedro Sampaio, Mc Pedrinho",
+        cover: "imagemPadrao.png",
+        file: "01 - PEDRO SAMPAIO, MC Pedrinho - DANÇARINA.mp3"
+      },
+    //Sexta
+      {
+        title: "Malvada",
+        artist: "Zé Felipe",
+        cover: "imagemPadrao.png",
+        file: "02 - Zé Felipe - Malvada.mp3"
+      },
+    //Setima
+      {
+        title: "Malvadão 3",
+        artist: "Xamã, Gustah, Neo Beats",
+        cover: "imagemPadrao.png",
+        file: "12 - Xamã, Gustah, Neo Beats - Malvadão 3.mp3"
+      },
+    //Oitava
+      {
+        title: "Bloqueado (Ao Vivo)",
+        artist: "Gusttavo Lima",
+        cover: "imagemPadrao.png",
+        file: "06 - Gusttavo Lima - Bloqueado - Ao Vivo.mp3"
+      },
+    //Nona
+      {
+        title: "Adivinha Aí",
+        artist: "Zé Vaqueiro",
+        cover: "imagemPadrao.png",
+        file: "20 - Zé Vaqueiro - Adivinha Aí.mp3"
+      },
+    //Decima
+      {
+        title: "212",
+        artist: "Chefin, Mainstreet",
+        cover: "imagemPadrao.png",
+        file: "24 - Chefin, Mainstreet - 212.mp3"
+      },
+    //Decima primeira
+      {
+        title: "Fora dos Stories",
+        artist: "Turma do Pagode",
+        cover: "imagemPadrao.png",
+        file: "28 - Turma do Pagode - Fora dos Stories.mp3"
+      },
+    //Decima segunda
+      {
+        title: "Senta Danada",
+        artist: "Zé Felipe, Os Barões da Pisadinha",
+        cover: "imagemPadrao.png",
+        file: "77 - Zé Felipe, Os Barões da Pisadinha - Senta Danada.mp3"
+      }
+
+  ];
+  
---
+++ protejoSpotify/source/index.js
@@ -0,0 +1,7 @@
+import player from "./player.js";
+
+
+
+window.addEventListener("load", () => {
+  player.start();
+});
